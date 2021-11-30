0c8ca877aa8525d53dacf64d9d38b0074b7aeab7
Cedric Smith <cedricsmith@Cedrics-MacBook-Air.local>
2021-11-30 11:39:16 -0700

    updated

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,31 +1,39 @@
-import React, { Component } from 'react';
+import React, {useState, useEffect} from 'react';
 import CardList from '../components/CardList';
 import SearchBox from '../components/SearchBox';
 import Scroll from '../components/Scroll';
 import ErrorBoundry from '../components/ErrorBoundry';
 import './App.css'
 
-class App extends Component {
-    constructor() {
-        super() //super calls the constructor of the component
-        this.state = {
-            jordans: [],
-            searchfield: ''
-        }
-    }
+function App() {
+    // constructor() {
+    //     super() //super calls the constructor of the component
+    //     this.state = {
+    //         jordans: [],
+    //         searchfield: ''
+    //     }
+    // }
+    const [jordans, setJordans] = useState([]);
+    const [searchfield, setSearchfield] = useState('');
+    const [count, setCount] = useState(0);
 
-    componentDidMount() {
+    // componentDidMount() {
+    //     fetch('https://jsonplaceholder.typicode.com/users')
+    //     .then(response => response.json())
+    //     .then(users => this.setState({ jordans: users }));
+        
+    // }
+    useEffect(() => {
         fetch('https://jsonplaceholder.typicode.com/users')
         .then(response => response.json())
-        .then(users => this.setState({ jordans: users }));
-        
-    }
+        .then(users => {setJordans(users)});
+        console.log(count);
+    },[count]); //only run if count changes.
 
-    onSearchChange = (event) => { //anytime you use this. use arrow functions
-        this.setState({ searchfield: event.target.value }) //onsearchchange is a function to help with the communication.
+    const onSearchChange = (event) => { //anytime you use this. use arrow functions
+        setSearchfield(event.target.value) //onsearchchange is a function to help with the communication.
     }
-    render() {
-        const { jordans, searchfield } = this.state;
+        
         const filterJordans = jordans.filter(jordan => {
             return jordan.name.toLowerCase().includes(searchfield.toLowerCase());
         })
@@ -34,7 +42,8 @@ class App extends Component {
           (
             <div className='tc'>
                 <h1 className='f1'>Air Jordans</h1>
-                <SearchBox searchChange={this.onSearchChange}/>
+                <button onClick={()=>setCount(count+1)}>Click Me!</button>
+                <SearchBox searchChange={onSearchChange}/>
                 <Scroll>
                     <ErrorBoundry>
                     <CardList jordans={filterJordans}/>
@@ -43,7 +52,7 @@ class App extends Component {
             </div>
         )
         }
-    }
+    
 
 export default App;
 
